7b5029c0db18b25f96da3c0b3c7a9fa20370bcf0
Knowledge Brown <knowledgexbrown@gmail.com>
2019-03-15 10:10:01 -0500

    SSO Auth

---
+++ frontend/build/precache-manifest.5b2da12d0a885bab37a04c16286a9c78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9b8e0090d28f5c8f32ce6c79c72f1f1",
+    "url": "/static/media/logo.c9b8e009.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "616dfcc7db24511faaac",
+    "url": "/static/js/main.616dfcc7.chunk.js"
+  },
+  {
+    "revision": "5b2b83c10488065c2e24",
+    "url": "/static/js/2.5b2b83c1.chunk.js"
+  },
+  {
+    "revision": "616dfcc7db24511faaac",
+    "url": "/static/css/main.25aaf38f.chunk.css"
+  },
+  {
+    "revision": "5b2b83c10488065c2e24",
+    "url": "/static/css/2.86b21696.chunk.css"
+  },
+  {
+    "revision": "a3eb5308b889e03278c01a0a128dbe96",
+    "url": "/index.html"
+  }
+];
